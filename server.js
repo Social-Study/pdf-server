4c754a3878bf7406f8c51c608b8b68500f9440fe
Evan Buss <evan.buss28@gmail.com>
2019-04-11 23:37:51 -0400

    Added custom styling and a prototype webpage to test API

--- server.js
+++ server.js
@@ -7,23 +7,32 @@ const markdownpdf = require('markdown-pdf');
 const fs = require('fs');
 const app = express();
 
+// Request body is converted to json format
 app.use(express.json());
+
+// Necessary for debugging but not sure if needed in production.
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.post('/', (req, res) => {
 
   const fileName = req.body.title + ".pdf";
-  markdownpdf().from.string(req.body.content).to(fileName, () => {
+  markdownpdf({ cssPath: 'githubStyle.css' }).from.string(req.body.content).to(fileName, () => {
     res.download(fileName, (err) => {
       if (err) {
         console.error("File transfer failure.");
+        console.log(err);
       } else {
+        // Delete file when finished
         fs.unlink(fileName, (err) => {
           if (err) console.log(err);
         })
       }
     });
   })
-
 })
 
 app.listen(3000, () => console.log("Server Running"));
